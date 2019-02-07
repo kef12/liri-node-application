6f71a104230df794e178555e39d998f014eaf8cd
Kate <kef12@comcast.net>
2019-02-07 16:38:17 -0700

    beautify

--- liri.js
+++ liri.js
@@ -85,7 +85,7 @@ switch (command) {
 
         break;
 
-//Switch case statement for 'spotify-this-song' command. 
+        //Switch case statement for 'spotify-this-song' command. 
 
     case "spotify-this-song":
 
@@ -156,18 +156,18 @@ switch (command) {
                             console.log("Spotify Preview Link: " + songInfo.preview_url);
 
                             console.log("Album: " + songInfo.album.name);
-                                }
+                        }
 
-                            });
-                        };
+                    });
+            };
         }
 
         break;
 
 
-    //Switch case statement for 'movie-this' command.    
-        case "movie-this" :
-        
+        //Switch case statement for 'movie-this' command.    
+    case "movie-this":
+
         if (movie == null) {
 
             axios.get("https://omdbapi.com/?t=mr.nobody&apikey=trilogy")
@@ -186,63 +186,63 @@ switch (command) {
                 }, function (error) {
 
                     if (error.resp) {
-                        
+
                         console.log(error.resp.data);
                         console.log(error.resp.status);
                         console.log(error.resp.headers);
 
                     } else if (error.request) {
-                       
+
                         console.log(error.request);
 
                     } else {
-                        
+
                         console.log('Error', error.message);
                     }
-                        console.log(error.config);
+                    console.log(error.config);
                 })
 
         } else {
 
-            axios.get("https://omdbapi.com/?t=" + movie  + "&apikey=trilogy")
-            
-            .then(function(resp) {
-            
-                console.log("Movie Title: " + resp.data.Title);
-                console.log("Year Released: " + resp.data.Year);
-                console.log("IMDB Rating: " + resp.data.imdbRating);
-                console.log("Rotten Tomatoes Rating: " + resp.data.Ratings[1].Value);
-                console.log("Country Produced: " + resp.data.Country);
-                console.log("Language of the Movie: " + resp.data.Language);
-                console.log("Movie Plot: " + resp.data.Plot);
-                console.log("Movie Actors: " + resp.data.Actors);
+            axios.get("https://omdbapi.com/?t=" + movie + "&apikey=trilogy")
 
-            }, function(error) {
+                .then(function (resp) {
 
-                if (error.resp) {
-                    
-                    console.log(error.resp.data);
-                    console.log(error.resp.status);
-                    console.log(error.resp.headers);
+                    console.log("Movie Title: " + resp.data.Title);
+                    console.log("Year Released: " + resp.data.Year);
+                    console.log("IMDB Rating: " + resp.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + resp.data.Ratings[1].Value);
+                    console.log("Country Produced: " + resp.data.Country);
+                    console.log("Language of the Movie: " + resp.data.Language);
+                    console.log("Movie Plot: " + resp.data.Plot);
+                    console.log("Movie Actors: " + resp.data.Actors);
 
-                } else if (error.request) {
-                
-                    console.log(error.request);
+                }, function (error) {
 
-                } else {
-                    
-                    console.log('Error', error.message);
+                    if (error.resp) {
 
-                }
+                        console.log(error.resp.data);
+                        console.log(error.resp.status);
+                        console.log(error.resp.headers);
 
-                console.log(error.config);
-            })
+                    } else if (error.request) {
+
+                        console.log(error.request);
+
+                    } else {
+
+                        console.log('Error', error.message);
+
+                    }
+
+                    console.log(error.config);
+                })
         }
 
-    break;
+        break;
 
-// Switch case statement for 'do-what-it-says' command.
-case "do-what-it-says" :
+        // Switch case statement for 'do-what-it-says' command.
+    case "do-what-it-says":
         fs.readFile("random.txt", "utf8", function (error, data) {
 
             if (error) {
@@ -255,51 +255,51 @@ case "do-what-it-says" :
 
             spotify.search({
 
-                type: "track",
+                    type: "track",
 
-                query: song,
+                    query: song,
 
-                limit: 1
-            },
+                    limit: 1
+                },
 
-            function (err, data) {
+                function (err, data) {
 
-                if (err) {
+                    if (err) {
 
-                    console.log('Error occurred: ' + err);
+                        console.log('Error occurred: ' + err);
 
-                    return;
+                        return;
 
-                } else {
+                    } else {
 
-                    var songInfo = data.tracks.items[0];
+                        var songInfo = data.tracks.items[0];
 
-                    var artists = songInfo.artists[0].name;
+                        var artists = songInfo.artists[0].name;
 
-                    var songTitle = songInfo.name;
+                        var songTitle = songInfo.name;
 
-                    var songUrl = songInfo.preview_url;
+                        var songUrl = songInfo.preview_url;
 
-                    var songAlbum = songInfo.album.name;
+                        var songAlbum = songInfo.album.name;
 
-                    console.log("Artist(s): " + artists);
+                        console.log("Artist(s): " + artists);
 
-                    console.log("The Song's Name: " + songInfo.name);
+                        console.log("The Song's Name: " + songInfo.name);
 
-                    console.log("Spotify Preview Link: " + songInfo.preview_url);
+                        console.log("Spotify Preview Link: " + songInfo.preview_url);
 
-                    console.log("Album: " + songInfo.album.name);
+                        console.log("Album: " + songInfo.album.name);
 
-                };
+                    };
 
-            })
-            
+                })
+
         });
-        
-    break;
 
-    default : 
+        break;
+
+    default:
 
         console.log("I'm so sorry, but your search returned no valuable results...");
-    
-} 
+
+}
